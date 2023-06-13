93b7a49c077a15ade6658f4386755943fd10998f
sh6137 <33920015+sh6137@users.noreply.github.com>
2023-06-13 17:39:09 +0900

    Initial commit

---
+++ src/test/java/com/example/restaurant/naver/NaverClientTest.java
@@ -0,0 +1,25 @@
+package com.example.restaurant.naver;
+
+import com.example.restaurant.naver.dto.SearchLocalReq;
+import com.example.restaurant.naver.dto.SearchLocalRes;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class NaverClientTest {
+    @Autowired
+    private NaverClient naverClient;
+
+    @Test
+    public void localSearchTest(){
+        var search = new SearchLocalReq();
+        search.setQuery("갈비집");
+
+        SearchLocalRes result = naverClient.searchLocal(search);
+
+        System.out.println(result);
+    }
+
+}
